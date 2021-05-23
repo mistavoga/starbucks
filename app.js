0b2e43185feeda2dc1af06d5e05d72c3f223d9c3
Nicolas von Garrel <9nineman9@googlemail.com>
2021-05-23 14:38:15 +0200

    starbucks clone skeleton

---
+++ app.js
@@ -0,0 +1,10 @@
+let navlinks = document.getElementById("navbar");
+let links = navlinks.getElementsByClassName("link");
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
