b26af6e27cd9a2d8bb88ec51d9ba60a089c3caa7
Nicolas von Garrel <9nineman9@googlemail.com>
2021-06-18 16:48:22 +0200

    change

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 let navlinks = document.getElementById("navbar");
 let links = navlinks.getElementsByClassName("link");
 
+let list = document.getElementsById("ul");
+let listelement = list.getElementsByClassName("li");
+
 for (let i = 0; i < links.length; i++) {
   links[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+for (let i = 0; i < listelement.length; i++) {
+  listelement[i].addEventListener("click", function () {
     let current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
