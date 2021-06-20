a1ceb16a6bf6435c0ec79ca1642ebf1f72c17d8b
Nicolas von Garrel <9nineman9@googlemail.com>
2021-06-20 22:20:39 +0200

    responsive corrections

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 let navlinks = document.getElementById("navbar");
 let links = navlinks.getElementsByClassName("link");
 
+let ul = document.querySelector("ul");
+let li = ul.querySelectorAll("li");
+
+for (let i = 0; i < li.length; i++) {
+  li[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active")[0].id;
+    current = current.replace("active", "");
+    this.className += " active";
+  });
+}
+
 for (let i = 0; i < links.length; i++) {
   links[i].addEventListener("click", function () {
     let current = document.getElementsByClassName("active");
